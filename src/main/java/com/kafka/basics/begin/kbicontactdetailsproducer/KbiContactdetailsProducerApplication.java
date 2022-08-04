f09a174119792719abc55a4a9e9e571d875cc391
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2022-08-04 16:26:54 -0500

    first commit

---
+++ src/main/java/com/kafka/basics/begin/kbicontactdetailsproducer/KbiContactdetailsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.basics.begin.kbicontactdetailsproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbiContactdetailsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbiContactdetailsProducerApplication.class, args);
+	}
+
+}
